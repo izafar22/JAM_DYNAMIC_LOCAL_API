49ff5b61fb29e2eedc1d20f7e9892b516b01a2fb
Imran Zafar <izafar22@gmail.com>
2021-05-02 04:35:45 +0530

    node-fetch added

--- src/_functions/assetapi.js
+++ src/_functions/assetapi.js
@@ -1,30 +1,41 @@
-const fetch = require("node-fetch");
-
+//const fetch = require("node-fetch");
+const ApolloClient = require('@apollo-boost').ApolloClient;
+let gql = require('@apollo-boost').gql;
+const uri = "https://d1wpvizvsyhfhp.cloudfront.net/cms/read/en-US";
 exports.handler = async function(event, context, callback) {
-    const response = await fetch("https://d1wpvizvsyhfhp.cloudfront.net/cms/read/en-US", {
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            Authorization: `Bearer ${process.env.ASSET_API_KEY}`
-        },
-        body: JSON.stringify({
-            query: `{
-                listAssets{
-                    data{
-                      title
-                      bedrooms
-                    }
-                  }
-          }`,
-        }),
+    const client = new ApolloClient({
+        uri,
+        Authorization: "Bearer a71230612484f77f14bc9621bbd8736da89d0e5fc684abdc"
     });
 
-    console.log("the limra response", JSON.stringify(response));
-    if (response.errors) {
-        callback(response.errors);
-    }
+    const query = gql `
+                    query {
+                        listAssets {
+                            data {
+                                title
+                                bedrooms
+                            }
+                        }
+                    }
+                    `;
+
+    console.log("the limra response");
+
+    client
+        .query({ query })
+        .then((response) => {
+            console.log("the response came", response);
+            console.log("the response came with data", response.data);
+            return `
+                    total_Assets: $ { response.data }
+                    `;
+        })
+        .then(console.log)
+        .catch(console.error);
     callback(null, {
         statusCode: 200,
-        body: JSON.stringify(response.data)
+        body: JSON.stringify({
+            message: "data"
+        })
     })
 }
