6380776778efe3267f7f7749ec5e91085df92830
Imran Zafar <izafar22@gmail.com>
2021-05-02 03:06:35 +0530

    node-fetch added

--- src/_functions/assetapi.js
+++ src/_functions/assetapi.js
@@ -1,4 +1,4 @@
-const url = "https://d1wpvizvsyhfhp.cloudfront.net/cms/read/en-US";
+const fetch = require("node-fetch");
 
 exports.handler = async function(event, context, callback) {
     const response = await fetch("https://d1wpvizvsyhfhp.cloudfront.net/cms/read/en-US", {
