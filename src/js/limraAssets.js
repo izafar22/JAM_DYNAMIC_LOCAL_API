4d80ec692d6fd65da786579895841013c41d718c
Imran Zafar <izafar22@gmail.com>
2021-05-02 03:49:24 +0530

    node-fetch added

--- src/js/limraAssets.js
+++ src/js/limraAssets.js
@@ -2,10 +2,8 @@ const url = `/.netlify/functions/assetapi`;
 
 fetch(url)
     .then((response) => {
-        console.log("response", response);
-        return response.json();
-    }).then((data) => {
-        console.log("the data limra", data);
+        console.log("response", response.data);
+        return response.data;
     }).catch((error) => {
         console.log("error", error);
     })
