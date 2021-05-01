d6211366895ced58dbedd3a9588a14ca6f8a42bd
Imran Zafar <izafar22@gmail.com>
2021-05-02 03:41:16 +0530

    node-fetch added

--- src/_functions/assetapi.js
+++ src/_functions/assetapi.js
@@ -4,7 +4,6 @@ exports.handler = async function(event, context, callback) {
     const response = await fetch("https://d1wpvizvsyhfhp.cloudfront.net/cms/read/en-US", {
         method: "GET",
         headers: {
-            "Content-Type": "application/json",
             Accept: "application/json",
             Authorization: `Bearer ${process.env.ASSET_API_KEY}`
         },
