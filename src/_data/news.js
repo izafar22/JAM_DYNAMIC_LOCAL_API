b0acd0b6a5ff1e30d313a3a460d15652e0601ec0
Imran Zafar <izafar22@gmail.com>
2021-05-02 17:05:36 +0530

    graphql added

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,29 +1,27 @@
-
-const axios = require('axios');
-require('dotenv').config();
-const countries = require('./countries.json');
-
-
-async function getNews(country) {
-  try {
-    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
-    return {
-      country:country,
-      articles:response.data.articles,
-    };
-  } catch (error) {
-    console.error(error);
-  }
-}
+// const axios = require('axios');
+// require('dotenv').config();
+// const countries = require('./countries.json');
 
 
-module.exports = async function(){
-  const newsPromises = countries.map(getNews);
-  return Promise.all(newsPromises)
-  .then(newsObjects=>{
-    console.log("newsObjects:",newsObjects);
-    return [].concat.apply([], newsObjects);
-  })
+// async function getNews(country) {
+//   try {
+//     const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
+//     return {
+//       country:country,
+//       articles:response.data.articles,
+//     };
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
 
-};
 
+// module.exports = async function(){
+//   const newsPromises = countries.map(getNews);
+//   return Promise.all(newsPromises)
+//   .then(newsObjects=>{
+//     console.log("newsObjects:",newsObjects);
+//     return [].concat.apply([], newsObjects);
+//   })
+
+// };
