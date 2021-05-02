664981ba2c4cd4d2f49b16289fe2b5de23351d56
Imran Zafar <izafar22@gmail.com>
2021-05-02 17:21:58 +0530

    graphql added

--- src/_functions/assetapi.js
+++ src/_functions/assetapi.js
@@ -16,20 +16,23 @@ exports.handler = async function(event, context, callback) {
     console.log("the url", uri);
 
     fetch(uri, {
-            method: 'POST',
+            method: 'post',
             body: data,
             headers: {
                 'Content-Type': 'application/json',
+                'Content-Length': data.length,
                 'Authorization': 'Bearer a71230612484f77f14bc9621bbd8736da89d0e5fc684abdc'
             }
         }).then(res => {
+            console.log("I got called");
             callback(null, {
                 statusCode: 200,
                 body: JSON.stringify(res.data)
             })
         })
         .catch(err => {
-            console.log(err)
+            console.log("I got called error");
+            console.log(err);
             callback(err, {
                 statusCode: 500,
                 body: JSON.stringify({
--- src/js/limraAssets.js
+++ src/js/limraAssets.js
@@ -1,9 +1,13 @@
-const url = `/.netlify/functions/assetapi`;
+(function() {
+    const url = `/.netlify/functions/assetapi`;
 
-fetch(url).then((response) => {
-    console.log("response", response);
-    return response.json();
-}).then((data) => {
-    console.log("data Study", data);
-    console.log("data monitoring", data.data);
-});
+    fetch(url).then((response) => {
+        console.log("response", response);
+        return response.json();
+    }).then((data) => {
+        console.log("data Study", data);
+        console.log("data monitoring", data.data);
+    }).catch((err) => {
+        console.log("the error", err);
+    })
+})();
